7b7864e1cf693e0be174839467d8ab9a6ab7c3cc
Ramzza <thezniqer@gmail.com>
2023-07-22 22:02:16 +0300

    [GH-5] feat: use json file as input (#10)

--- src/app.js
+++ src/app.js
@@ -1,14 +1,16 @@
 import dotenv from 'dotenv';
 import copyFile from './utils/copyFile.js';
+import readInputFile from './utils/readInputFile.js';
 
 dotenv.config();
 
 export default function main() {
   try {
-    copyFile(
-      process.env.INPUT_SOURCE_FILE,
-      process.env.OUTPUT_DESTINATION_FILE
-    );
+    const inputJson = readInputFile();
+
+    inputJson.forEach((fileToCopy) => {
+      copyFile(fileToCopy.src, fileToCopy.dest);
+    });
     console.log('Success!');
   } catch (err) {
     console.error(err);
